1c717270ce2f368258c6600c71fed8e633175a9a
Victor Foreman <vforeman@scs.howard.edu>
2014-09-13 14:38:27 -0400

    pulled ej only

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-//server
+// server.js
+
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get("/", function(req,res){
+	res.sendfile('app/base.html');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function(){
+	console.log("Listening on " + port);
+});
